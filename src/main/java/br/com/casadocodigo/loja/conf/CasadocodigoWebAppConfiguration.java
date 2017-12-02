8dd6a56cc48c210451bb5636a4e59c393d365b80
Rafael Rollo <rafaelrollo92@gmail.com>
2017-12-02 14:43:27 -0200

    COnfigurando app Spring MVC

---
+++ src/main/java/br/com/casadocodigo/loja/conf/CasadocodigoWebAppConfiguration.java
@@ -0,0 +1,19 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages={"br.com.casadocodigo.loja"})
+public class CasadocodigoWebAppConfiguration {
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
---
+++ src/main/java/br/com/casadocodigo/loja/conf/SpringMvcInitializer.java
@@ -0,0 +1,22 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {CasadocodigoWebAppConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
