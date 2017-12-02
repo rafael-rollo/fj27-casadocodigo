c81cc685c9cdd123f297b2cfea707e2760a479e6
Rafael Rollo <rafaelrollo92@gmail.com>
2017-12-02 16:13:06 -0200

    Recebendo produto no controller
    Exercício 3.3

--- src/main/java/br/com/casadocodigo/loja/conf/CasadocodigoWebAppConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/CasadocodigoWebAppConfiguration.java
@@ -2,12 +2,14 @@ package br.com.casadocodigo.loja.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @ComponentScan(basePackages={"br.com.casadocodigo.loja"})
-public class CasadocodigoWebAppConfiguration {
+public class CasadocodigoWebAppConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
@@ -15,5 +17,10 @@ public class CasadocodigoWebAppConfiguration {
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
 	}
 }
